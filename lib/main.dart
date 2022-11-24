4b494537c09dc62fe3fc7ba490006afa3351aaa9
boogahead <wherestuffgoes2@gmail.com>
2022-11-24 10:00:33 +0900

    test boogahead

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,16 @@ import 'package:centerfascia_application/pages/google_maps.dart';
 import 'package:centerfascia_application/pages/youtube_playlist.dart';
 import 'package:centerfascia_application/pages/logo.dart';
 
-
-
-import 'package:flutter/material.dart';
-
 void main() {
   runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute : '/',
-      routes:{/*
+      initialRoute: '/',
+      routes: {
+        /*
         '/home': (context) => Home(),
         '/youtube_playlist' : (context) => YoutubePlaylist(),
         '/hw_control' : (context) => HW_Control(),
         '/google_maps' : (context) => GoogleMaps(),
         //'/logo' : (context) => Logo(),*/
-      }
-  ));
+      }));
 }
