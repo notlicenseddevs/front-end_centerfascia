0711360144af83971cda550e4839cd4c1e7a323b
boogahead <wherestuffgoes2@gmail.com>
2022-11-25 11:07:24 +0900

    created nav rail

--- lib/pages/hw_control.dart
+++ lib/pages/hw_control.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class HW_Control extends StatefulWidget {
   const HW_Control({Key? key}) : super(key: key);
@@ -9,17 +10,144 @@ class HW_Control extends StatefulWidget {
 
 class _HW_ControlState extends State<HW_Control> {
   ////put stuff here//////
-
+  /// // Track the currently selected item here. Only used for
+  // tablet layouts.
   //////ye////
+  // The contents of views
+  // Only the content associated with the selected tab is displayed on the screen
+  final List<Widget> _mainContents = [
+    // Content for Home tab
+    Container(
+      color: Colors.grey[850],
+      alignment: Alignment.center,
+      child: const Text(
+        'Home',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Feed tab
+    Container(
+      color: Colors.grey[850],
+      alignment: Alignment.center,
+      child: const Text(
+        'Feed',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Favorites tab
+    Container(
+      color: Colors.grey[850],
+      alignment: Alignment.center,
+      child: const Text(
+        'Favorites',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Settings tab
+    Container(
+      color: Colors.grey[850],
+      alignment: Alignment.center,
+      child: const Text(
+        'Settings',
+        style: TextStyle(fontSize: 40),
+      ),
+    )
+  ];
+
+  // The index of the selected tab
+  // In the beginning, the Home tab is selected
+  int _selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0x303030),
+      // Show the bottom tab bar if screen width < 640
+      bottomNavigationBar: MediaQuery.of(context).size.width < 640
+          ? BottomNavigationBar(
+              currentIndex: _selectedIndex,
+              unselectedItemColor: Colors.grey,
+              selectedItemColor: Colors.indigoAccent,
+              // called when one tab is selected
+              onTap: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              // bottom tab items
+              items: const [
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home), label: 'Home'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.feed), label: 'Feed'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.favorite), label: 'Favorites'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.settings), label: 'Settings')
+                ])
+          : null,
+      body: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          // Show the navigaiton rail if screen width >= 640
+          if (MediaQuery.of(context).size.width >= 640)
+            NavigationRail(
+              backgroundColor: Colors.blue[800], //changes nav rail color
+              minWidth: 55.0,
+              selectedIndex: _selectedIndex,
+              // Called when one tab is selected
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              labelType: NavigationRailLabelType.all,
+              selectedLabelTextStyle: const TextStyle(
+                color: Colors.amber,
+              ),
+              leading: Column(
+                children: const [
+                  SizedBox(
+                    height: 8,
+                  ),
+                  CircleAvatar(
+                    radius: 20,
+                    child: Icon(Icons.person),
+                  ),
+                ],
+              ),
+              unselectedLabelTextStyle: const TextStyle(),
+              // navigation rail items
+              destinations: const [
+                NavigationRailDestination(
+                    icon: Icon(Icons.home), label: Text('Home')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.feed), label: Text('Feed')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.favorite), label: Text('Favorites')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.settings), label: Text('Settings')),
+              ],
+            ),
+
+          // Main content
+          // This part is always shown
+          // You will see it on both small and wide screen
+          Expanded(child: _mainContents[_selectedIndex]),
+        ],
+      ),
+    );
+  }
+  //return Scaffold(
+  /*appBar: AppBar(
+          backgroundColor: Colors.black,
           centerTitle: true,
           elevation: 0,
           leading: Container(),
           title: Text('HW_Control'),
-        ),
-        body: Container());
-  }
+        ),*/
+  //body:Scaffold(
+
+  //),
+  //color: Colors.grey[800],
+  //);
 }
